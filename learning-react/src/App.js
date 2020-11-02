288907f56834f858dd4961f2011e9700a6ce815b
Terence <terencedumas@Terences-MacBook-Air.local>
2020-11-02 17:28:32 -0600

    new

---
+++ learning-react/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import HelloWorld from './Components/HelloWorld.js'
+import Navigation from './Components/Navigation.js'
+import Header from './Components/Header.js'
+import Footer from './Components/Footer.js'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <HelloWorld/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
